75a2182cd469a873bd227349c93983bba25fb8f5
kenzshelley <kenzshelley@gmail.com>
2016-01-02 19:36:52 -0500

    chagned back

--- app.js
+++ app.js
@@ -127,8 +127,7 @@ app.post('/queue_drink', function(req, res) {
     });
   });
 });
-process.env.OPENSHIFT_NODEJS_PORT = 8087
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8087
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port, server_ip_address, function () {
